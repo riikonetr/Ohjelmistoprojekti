80d4d9707fbedea30debaed6933cb77780bf9d2d
donkkis <panu.aho@gmail.com>
2019-01-23 23:35:50 +0200

    Site reorder, implemented social feed, spotify, youtube and re-implemented blog

--- static-wireframe/assets/js/main.js
+++ static-wireframe/assets/js/main.js
@@ -25,3 +25,36 @@ $(function() {
 	});
 
 });
+
+/*#####################
+Additional jQuery (required)
+#####################*/
+$(document).ready(function(){
+
+	var clickEvent = false;
+	$('#carouselExampleControls').carousel({
+		interval:   4000
+	}).on('click', '.list-group li', function() {
+			clickEvent = true;
+			$('.list-group li').removeClass('active');
+			$(this).addClass('active');
+			console.log("click!")
+	}).on('slid.bs.carousel', function(e) {
+		if(!clickEvent) {
+			var count = $('.list-group').children().length -1;
+			var current = $('.list-group li.active');
+			current.removeClass('active').next().addClass('active');
+			var id = parseInt(current.data('slide-to'));
+			if(count == id) {
+				$('.list-group li').first().addClass('active');
+			}
+		}
+		clickEvent = false;
+	});
+})
+$(window).load(function() {
+    var boxheight = $('#carouselExampleControls .carousel-inner').innerHeight();
+    var itemlength = $('#carouselExampleControls .carousel-item').length;
+    var triggerheight = Math.round(boxheight/itemlength+1);
+	$('#carouselExampleControls .list-group-item').outerHeight(triggerheight);
+});
