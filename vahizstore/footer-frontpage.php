af7f1ba2512308ab961e7fce638ea31f5b334356
donkkis <panu.aho@gmail.com>
2019-02-14 22:47:39 +0200

    css improvements, footer added

--- vahizstore/footer-frontpage.php
+++ vahizstore/footer-frontpage.php
@@ -8,9 +8,29 @@
  */
 
 ?>
+
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
         <footer role="contentinfo">
-            <!-- TODO: Social media links and some style here -->
-            <div>Footer text</div>
+          <div class="container">
+            <div class="row">
+              <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
+                <ul class="list-unstyled list-inline social text-center">
+                  <li class="list-inline-item"><a href="javascript:void();" target="_blank"><i class="fab fa-facebook-square"></i></a></li>
+                  <li class="list-inline-item"><a href="javascript:void();" target="_blank"><i class="fab fa-instagram"></i></a></li>
+                  <li class="list-inline-item"><a href="javascript:void();" target="_blank"><i class="fab fa-spotify"></i></a></li>
+                  <li class="list-inline-item"><a href="javascript:void();" target="_blank"><i class="fa fa-envelope"></i></a></li>
+                </ul>
+              </div>
+              </hr>
+            </div>
+            <div class="row">
+              <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
+                <p class="h6">&copy Gladenfold and the boyz 2019. Some rights reversed.<p>
+              </div>
+              </hr>
+            </div>
+          </div>
         </footer>
 
     </div><!-- #page -->
--- vahizstore/header-frontpage.php
+++ vahizstore/header-frontpage.php
@@ -14,6 +14,10 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=2.0">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<!-- Font Awesome Icons -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+
 
 <?php wp_head(); ?>
 </head>
