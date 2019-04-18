0024486c9b636e1158858c6a6d8906aec283436b
riikonetr <34094325+riikonetr@users.noreply.github.com>
2019-04-18 11:49:22 +0300

    Band members styling

--- vahizstore/sections/bio-section.php
+++ vahizstore/sections/bio-section.php
@@ -26,15 +26,14 @@
     }
 ?>
 
-<div class="container">
-    <div id="band-members" class="bandmembers row no-gutters">
+<div class="row no-gutters">
 <?php
     $band_members= get_theme_mod('band_members', json_encode( array(/*The content from your default parameter or delete this argument if you don't want a default*/)) );
     /*This returns a json so we have to decode it*/
     $customizer_repeater_decoded = json_decode($band_members);
     foreach($customizer_repeater_decoded as $repeater_item){
 ?>
-        <div class="col-6 col-sm-6 col-md">
+        <div class="col">
             <figure class="member-container">
                 <img class="member-image" src="<?php echo $repeater_item->image_url ?>">
                 <div class="member-text"><?php echo $repeater_item->title ?></div>
@@ -43,5 +42,4 @@
 <?php
     }
 ?>
-    </div>
 </div>
